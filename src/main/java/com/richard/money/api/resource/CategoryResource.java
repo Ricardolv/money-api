24c8e90731bf8deb43ad93622ce06986bba9450a
Ricardolv <richardluizv@gmail.com>
2017-07-20 21:45:12 -0300

    Cadastrando nova categoria com POST

--- src/main/java/com/richard/money/api/resource/CategoryResource.java
+++ src/main/java/com/richard/money/api/resource/CategoryResource.java
@@ -3,10 +3,12 @@ package com.richard.money.api.resource;
 import com.richard.money.api.model.Category;
 import com.richard.money.api.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,6 +21,22 @@ public class CategoryResource {
     @GetMapping
     public List<Category> listAll() {
         return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Category category, HttpServletResponse response) {
+        Category categorySave = categoryRepository.save(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{code}")
+                .buildAndExpand(categorySave.getCode()).toUri();
+
+        response.setHeader("Location", uri.toASCIIString());
+    }
+
+    @GetMapping("/{code}")
+    public Category searchCategoryByCode(@PathVariable Long code) {
+        return categoryRepository.findOne(code);
     }
 
 }
