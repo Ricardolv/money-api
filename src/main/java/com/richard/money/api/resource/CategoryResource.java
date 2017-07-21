77874d488b0028db6c76231788f201810b02f14d
Ricardolv <richardluizv@gmail.com>
2017-07-20 21:55:38 -0300

    tratamento retorna 404 de categoria

--- src/main/java/com/richard/money/api/resource/CategoryResource.java
+++ src/main/java/com/richard/money/api/resource/CategoryResource.java
@@ -2,8 +2,10 @@ package com.richard.money.api.resource;
 
 import com.richard.money.api.model.Category;
 import com.richard.money.api.repository.CategoryRepository;
+import com.richard.money.api.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -15,28 +17,32 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryResource {
 
+
     @Autowired
-    private CategoryRepository categoryRepository;
+    private CategoryService categoryService;
 
     @GetMapping
     public List<Category> listAll() {
-        return categoryRepository.findAll();
+        return categoryService.findAll();
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody Category category, HttpServletResponse response) {
-        Category categorySave = categoryRepository.save(category);
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Category>  create(@RequestBody Category category, HttpServletResponse response) {
+        Category categorySave = categoryService.save(category);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{code}")
                 .buildAndExpand(categorySave.getCode()).toUri();
 
         response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categorySave);
     }
 
     @GetMapping("/{code}")
-    public Category searchCategoryByCode(@PathVariable Long code) {
-        return categoryRepository.findOne(code);
+    public ResponseEntity<Category> searchCategoryByCode(@PathVariable Long code) {
+        Category category = categoryService.findOne(code);
+        return category != null ? ResponseEntity.ok(category) : ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/com/richard/money/api/service/CategoryService.java
+++ src/main/java/com/richard/money/api/service/CategoryService.java
@@ -1,6 +1,28 @@
 package com.richard.money.api.service;
 
 
+import com.richard.money.api.model.Category;
+import com.richard.money.api.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findOne(Long code) {
+        return categoryRepository.findOne(code);
+    }
 }
