f5b5fc5059c03c1b4975eb5eec991261f270d63a
Ricardolv <richardluizv@gmail.com>
2017-08-15 15:13:08 -0300

    Movendo o refresh token do cookie para a requisição

---
+++ src/main/java/com/richard/money/api/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,63 @@
+package com.richard.money.api.token;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        if ("/oauth/token".equalsIgnoreCase(req.getRequestURI())
+                && "refresh_token".equals(req.getParameter("grant_type"))
+                && null != req.getCookies()) {
+
+            for (Cookie cookie : req.getCookies()) {
+                if (cookie.getName().equals("refreshToken")) {
+                    String refreshToken = cookie.getValue();
+                    req = new MyServleRequestWrapper(req, refreshToken);
+                }
+            }
+        }
+
+        chain.doFilter(req, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException { }
+
+    @Override
+    public void destroy() { }
+
+    static class MyServleRequestWrapper extends HttpServletRequestWrapper {
+
+        private String refresToken;
+
+        public MyServleRequestWrapper(HttpServletRequest request, String refresToken) {
+            super(request);
+            this.refresToken = refresToken;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+            map.put("refresh_token", new String[] {refresToken});
+            map.setLocked(true);
+            return map;
+        }
+    }
+}
