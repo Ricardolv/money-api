9040cb1228fa21de148d129680da1d8589728fc2
Ricardolv <richardluizv@gmail.com>
2017-08-15 14:13:51 -0300

    Renovando o access token com o refresh token

--- src/main/java/com/richard/money/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/richard/money/api/config/AuthorizationServerConfig.java
@@ -28,9 +28,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("angular")
                 .secret("@ngul@r0")
                 .scopes("read", "write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(1800);
-
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(20)
+                .refreshTokenValiditySeconds(3600 * 24);
     }
 
     @Override
@@ -39,8 +39,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints
                .tokenStore(tokenStore())
                .accessTokenConverter(accessTokenConverter())
+               .reuseRefreshTokens(false)
                .authenticationManager(authenticationManager);
-
     }
 
     @Bean
