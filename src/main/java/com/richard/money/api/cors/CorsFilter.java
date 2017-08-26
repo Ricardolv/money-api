83be6e295f796fcdf311e9f631fe2a5ccb296885
Ricardolv <richardluizv@gmail.com>
2017-08-26 18:34:31 -0300

    ajustando os profiles

--- src/main/java/com/richard/money/api/cors/CorsFilter.java
+++ src/main/java/com/richard/money/api/cors/CorsFilter.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@Component
-//@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
     @Autowired
@@ -22,21 +22,21 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
 
-//        HttpServletRequest request = (HttpServletRequest) req;
-//        HttpServletResponse response = (HttpServletResponse) resp;
-//
-//        response.setHeader("Access-Control-Allow-Origin", moneyApiProperty.getEnvironment().getAllowedOrigin());
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//
-//        if ("OPTIONS".equals(request.getMethod()) && moneyApiProperty.getEnvironment().getAllowedOrigin().equals(request.getHeader("Origin"))) {
-//            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-//            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-//            response.setHeader("Access-Control-Max-Age", "3600");
-//
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            chain.doFilter(req, resp);
-//        }
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        response.setHeader("Access-Control-Allow-Origin", moneyApiProperty.getEnvironment().getAllowedOrigin());
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equals(request.getMethod()) && moneyApiProperty.getEnvironment().getAllowedOrigin().equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
 
     }
 
